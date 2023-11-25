5f06b46e77dd21b56919e5381a3013a44ad312cb
gonddrico <151998133+gonddrico@users.noreply.github.com>
2023-11-25 17:28:24 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+// Fungsi untuk mengubah warna latar belakang menjadi #9dede4
+function changeBackgroundColor() {
+  document.body.style.backgroundColor = '#9dede4';
+}
+
+// Panggil fungsi secara otomatis saat halaman dimuat
+changeBackgroundColor();
